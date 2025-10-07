2dfcb0b6021ba9f381e85041c3767a1c4ba8a7c0
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2025-10-07 14:37:24 -0600

    Working Scheme implementation.

--- scheme/super-duper.scm
+++ scheme/super-duper.scm
@@ -6,9 +6,25 @@
 
 ;; Main function: super-duper
 (define (super-duper source count)
-    (if (not (list? source))
-        source
+  (cond
+    ((not (list? source)) source)
+    ((null? source) '())
+    (else
+     (let loop ((dups (duper (super-duper (car source) count) count))
+                (rest (super-duper (cdr source) count)))
+       (if (null? dups)
+           rest
+           (cons (car dups) (loop (cdr dups) rest)))))))
 
-    )
 
-)
+;; Example usage
+(display (super-duper 123 1))
+(display (super-duper 123 2))
+(display (super-duper '() 1))
+(display (super-duper '() 2))
+(display (super-duper '(x) 1))
+(display (super-duper '(x) 2))
+(display (super-duper '(x y) 1))
+(display (super-duper '(x y) 2))
+(display (super-duper '((a b) y) 3))
+(display "\n")
