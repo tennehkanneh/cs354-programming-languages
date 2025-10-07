2a1a688b19403e161c5cb071b69f4e4b584bd600
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2025-10-07 14:25:18 -0600

    Completed duper method

--- scheme/super-duper.scm
+++ scheme/super-duper.scm
@@ -1,12 +1,14 @@
 ;; Duplicates a single element 'count' times
 (define (duper element count)
-
-
-)
+    (if (= count 0)
+        '()
+        (cons element (duper element (- count 1)))))
 
 ;; Main function: super-duper
 (define (super-duper source count)
+    (if (not (list? source))
+        source
 
-
+    )
 
 )
